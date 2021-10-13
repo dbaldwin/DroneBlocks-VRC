85f9fb8a20b94277577d121848905ef284338bb9
Dennis Baldwin <dennisbaldwin@gmail.com>
2021-10-13 09:20:23 -0500

    Some refactoring

--- src/custom_blocks/led_ring.js
+++ src/custom_blocks/led_ring.js
@@ -1,31 +1,67 @@
+import * as Blockly from 'blockly';
 import Utils from "../utils";
 
 export default class LEDRing {
-    constructor(blockly) {
-        blockly.defineBlocksWithJsonArray([
+    constructor() {
+        Blockly.defineBlocksWithJsonArray([
             {
-            "type": "led_color",
-            "message0": "Set LED Color %1",
-            "args0": [
-                {
-                "type": "field_colour",
-                "name": "rgb",
-                "colour": "#ff0000"
-                }
-            ],
-            "previousStatement": null,
-            "nextStatement": null,
-            "colour": 230,
-            "tooltip": "",
-            "helpUrl": ""
+                "type": "led_color",
+                "message0": "Set LED color %1",
+                "args0": [
+                    {
+                        "type": "field_colour",
+                        "name": "rgb",
+                        "colour": "#00ff00"
+                    }
+                ],
+                "previousStatement": null,
+                "nextStatement": null,
+                "colour": 230,
+                "tooltip": "",
+                "helpUrl": ""
+            },
+            {
+                "type": "led_color_delay",
+                "message0": "Set LED color %1 after %2 ms",
+                "args0": [
+                    {
+                        "type": "field_colour",
+                        "name": "rgb",
+                        "colour": "#00ff00"
+                    },
+                    {
+                        "type": "input_value",
+                        "name": "milliseconds"
+                    }
+                ],
+                "previousStatement": null,
+                "nextStatement": null,
+                "colour": 230,
+                "tooltip": "",
+                "helpUrl": ""
             }
         ]);
 
-        blockly.JavaScript['led_color'] = function(block) {
+        Blockly.JavaScript['led_color'] = function(block) {
             let hexColor = block.getFieldValue('rgb');
             let rgbColor = Utils.hexToRGB(hexColor);
             let rgbString = "[0," + rgbColor + "]";
-            return 'window.mqttClient.publish("vrc/pcc/set_base_color", \'{"wrgb": ' + rgbString + '}\')';
+            return 'window.mqttClient.publish("vrc/pcc/set_base_color", \'{"wrgb": ' + rgbString + '}\');';
         };
+
+        Blockly.JavaScript['led_color_delay'] = function(block) {
+            let hexColor = block.getFieldValue('rgb');
+            let rgbColor = Utils.hexToRGB(hexColor);
+            let rgbString = "[0," + rgbColor + "]";
+            let mqttPublishCommand = 'window.mqttClient.publish("vrc/pcc/set_base_color", \'{"wrgb": ' + rgbString + '}\');';
+            let delay = Blockly.JavaScript.valueToCode(block, 'milliseconds', Blockly.JavaScript.ORDER_NONE);
+            
+            return `setTimeout(function() { ${mqttPublishCommand} }, ${delay});`;
+            
+            
+            // let rgbString = "[0," + rgbColor + "]";
+            // return 'window.mqttClient.publish("vrc/pcc/set_base_color", \'{"wrgb": ' + rgbString + '}\');';
+        };
+
     }
 }
